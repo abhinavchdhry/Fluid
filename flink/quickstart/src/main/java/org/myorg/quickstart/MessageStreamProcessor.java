c337ec2141c62dae8b7f7154793f534263a83f8b
Abhinav Choudhury <abhinav155.bits@gmail.com>
2018-01-16 21:36:08 +0000

    Working changes for Flink: Testing basic map and sink write

--- flink/quickstart/src/main/java/org/myorg/quickstart/MessageStreamProcessor.java
+++ flink/quickstart/src/main/java/org/myorg/quickstart/MessageStreamProcessor.java
@@ -21,6 +21,8 @@ package org.myorg.quickstart;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
@@ -31,6 +33,7 @@ import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import java.util.Properties;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
+import org.myorg.quickstart.MessageObject;
 
 /**
  * Implements the "WordCount" program that computes a simple word occurrence histogram
@@ -45,9 +48,56 @@ import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.Obje
  * </ul>
  *
  */
+
+
 public class MessageStreamProcessor {
 
 	final static String TOPICNAME = "testtopic";
+/*
+	public static class MyReduceFunction implements ReduceFunction<ObjectNode> {
+		@Override
+		public ObjectNode reduce(ObjectNode n1, ObjectNode n2) throws Exception {
+			return new Tuple2<>(n1.get("key").asInt(), n1.get("value").asInt() + n2.get("value").asInt());
+		}
+	}
+*/
+/*
+	public final class MessageObject {
+
+        	public Integer id;
+        	public Integer value;
+
+                public MessageObject() {};
+                public MessageObject(Integer _id, Integer _value) { id = _id;  value = _value; }
+
+		public String toString()
+		{
+			Tuple2<Integer, Integer> tup = new Tuple2<Integer, Integer>(id, value);
+			return tup.toString();
+		}
+
+        };
+*/
+	public final class MyMapFunction implements MapFunction<ObjectNode, MessageObject> {
+        	@Override
+        	public MessageObject map(ObjectNode obj) throws Exception {
+                	return new MessageObject(obj.get("key").asInt(), obj.get("value").asInt());
+        	}
+	}
+
+	public final class MessageObjToStringMap implements MapFunction<MessageObject, String> {
+		@Override
+		public String map(MessageObject obj) throws Exception {
+			return obj.toString();
+		}
+	}
+
+	public final class MyReduceFunction implements ReduceFunction<MessageObject> {
+		@Override
+		public MessageObject reduce(MessageObject o1, MessageObject o2) throws Exception {
+			return new MessageObject(o1.id, o1.value + o2.value);
+		}
+	}
 
 	private static FlinkKafkaConsumer09[] getConsumerGroup(Integer numConsumers, String groupId) {
 		Properties properties = new Properties();
@@ -65,7 +115,10 @@ public class MessageStreamProcessor {
 	}
 
 	public static void main(String[] args) throws Exception {
+		new MessageStreamProcessor().start();
+	}
 
+	public void start() throws Exception {
 		// set up the execution environment
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
@@ -77,41 +130,36 @@ public class MessageStreamProcessor {
 
 //		FlinkKafkaConsumer09<ObjectNode> consumer = new FlinkKafkaConsumer09<>(TOPICNAME, new JSONKeyValueDeserializationSchema(false), properties);
 
-		DataStream<String> stream = env.addSource(new FlinkKafkaConsumer09<>(TOPICNAME, new SimpleStringSchema(), properties));
-//		stream.print();
+		DataStream<ObjectNode> stream = env.addSource(new FlinkKafkaConsumer09<>("jsontest21", new JSONDeserializationSchema(), properties));
+		stream.map(new MyMapFunction()).map(new MessageObjToStringMap()).writeAsText("/home/ubuntu/abhinav1.txt").setParallelism(1);
+//.reduce(new MyReduceFunction());
+//.map(new MessageObjToStringMap());
 
-		FlinkKafkaProducer09<String> myProducer = new FlinkKafkaProducer09<String>(
+//		stream.keyBy("key").reduce(
+//			new MyReduceFunction()
+/*			new ReduceFunction<Tuple2<Integer, Integer>>() {
+			
+			public Tuple2<Integer, Integer> reduce(ObjectNode n1, ObjectNode n2) throws Exception {
+				return new Tuple2<>(n1.get("key").asInt(), n1.get("value").asInt() + n2.get("value").asInt());
+			}
+		}
+*/
+//		).print();
+
+/*		FlinkKafkaProducer09<String> myProducer = new FlinkKafkaProducer09<String>(
         			"localhost:9092",            // broker list
         			"my-topic",                  // target topic
 			        new SimpleStringSchema());   // serialization schema
-
+*/
 		// versions 0.10+ allow attaching the records' event timestamp when writing them to Kafka;
 		// this method is not available for earlier Kafka versions
 //		myProducer.setWriteTimestampToKafka(true);
 
-		stream.addSink(myProducer);
+//		stream.addSink(myProducer);
 
 
 		env.execute("Stream processor");
 
-		// get input data
-/*		DataSet<String> text = env.fromElements(
-				"To be, or not to be,--that is the question:--",
-				"Whether 'tis nobler in the mind to suffer",
-				"The slings and arrows of outrageous fortune",
-				"Or to take arms against a sea of troubles,"
-				);
-
-		DataSet<Tuple2<String, Integer>> counts =
-				// split up the lines in pairs (2-tuples) containing: (word,1)
-				text.flatMap(new LineSplitter())
-				// group by the tuple field "0" and sum up tuple field "1"
-				.groupBy(0)
-				.sum(1);
-
-		// execute and print result
-		counts.print();
-*/
 	}
 
 	//
