caf1d742e596db57c633ae1ef516733ea6cf04a6
Abhinav <abhinav155.bits@gmail.com>
2018-01-25 05:27:59 +0000

    Renamed file

--- cassandra/create_tables.py
+++ cassandra/create_tables.py
@@ -12,5 +12,28 @@ create_table = """CREATE TABLE IF NOT EXISTS FINAL.OUTPUT_TABLE (
 
 session.execute(create_table)
 
+create_comments_table = """CREATE TABLE IF NOT EXISTS FINAL.COMMENTS_TABLE (
+	id text PRIMARY KEY,
+	thread_id text,
+	subreddit_id text,
+	author_id text,
+	parent_id text,
+	body text,
+	score text
+	)"""
+
+session.execute(create_comments_table)
+
+create_ads_table = """CREATE TABLE IF NOT EXISTS FINAL.ADS_TABLE (
+	id text PRIMARY KEY,
+	title text,
+	body text,
+	tags text
+	)"""
+
+session.execute(create_ads_table)
+
 # Clear the table if it exists
 session.execute("""TRUNCATE FINAL.OUTPUT_TABLE""")
+session.execute("""TRUNCATE FINAL.COMMENTS_TABLE""")
+session.execute("""TRUNCATE FINAL.ADS_TABLE""")
