35a2d24f23baca8c081a1c78a9f91ab56f578a97
Abhinav <abhinav155.bits@gmail.com>
2018-01-26 07:45:53 +0000

    Stabilisation and internal protocol fixes

--- cassandra/read_ads_data_into_cassandra.py
+++ cassandra/read_ads_data_into_cassandra.py
@@ -4,10 +4,10 @@ import json
 cluster = Cluster()
 session = cluster.connect()
 
-create_keyspace = "CREATE KEYSPACE IF NOT EXISTS ADS WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor':3}"
+create_keyspace = "CREATE KEYSPACE IF NOT EXISTS FINAL WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor':3}"
 session.execute(create_keyspace)
 
-create_table = """CREATE TABLE IF NOT EXISTS ADS.ADS_TABLE (
+create_table = """CREATE TABLE IF NOT EXISTS FINAL.ADS_TABLE (
 	id text PRIMARY KEY,
 	title text,
 	body text,
@@ -16,9 +16,9 @@ create_table = """CREATE TABLE IF NOT EXISTS ADS.ADS_TABLE (
 session.execute(create_table)
 
 def clear_table(session):
-	session.execute("TRUNCATE ADS.ADS_TABLE")
+	session.execute("TRUNCATE FINAL.ADS_TABLE")
 
-insert_statement_body = "INSERT INTO ADS.ADS_TABLE (id, title, body, tags) VALUES "
+prep = session.prepare("INSERT INTO FINAL.ADS_TABLE (id, title, body, tags) VALUES (?, ?, ?, ?)")
 
 clear_table(session)
 
@@ -28,13 +28,10 @@ for line in f:
 	j = json.loads(line)
 
 	session.execute(
-	"""
-	INSERT INTO ADS.ADS_TABLE (id, title, body, tags)
-	VALUES (%s, %s, %s, %s)
-	""",
+	prep,
 	(j["id"], j["title"], j["body"], j["tags"])
 	)
 
 	count += 1
-	if count == 10:
-		break
+
+print("Loaded " + str(count) + "rows")
